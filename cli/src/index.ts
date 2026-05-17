cd550a81e630163dd0e99a2a2a1d706f9e2ae742
wang_hongqi <wanghongqi@zlinkcloudtech.com>
2026-05-17 19:47:19 +0800

    chore: release 0.1.3 [skip ci]

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -18,7 +18,7 @@ const program = new Command();
 program
   .name("remote-host")
   .description("SSH/SCP tool for managing remote devices")
-  .version("0.1.2");
+  .version("0.1.3");
 
 registerConfig(program);
 registerCompletion(program);
